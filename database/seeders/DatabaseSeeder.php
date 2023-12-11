5dc769d6d37383d3bc10fdef7451a8ed50021cee
CendaCZ <thecendacz@gmail.com>
2023-12-11 17:10:06 +0100

    Created Models (Base details) + Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +19,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Country::create(['name' => 'Czechia', 'codename' => 'cs_CZ']);
+        City::create(['country_id' => 1, 'name' => 'Plzeň']);
+        City::create(['country_id' => 1, 'name' => 'Praha']);
+        Tag::create(['name' => 'Studentská akce', 'slug' => 'studentska']);
     }
 }
